e92aa5c56943484a722ec1a524c51282c6275111
gabriele-chiocca <gabrielechiocca00@gmail.com>
2026-05-16 14:18:16 +0200

    added thernary operator on the object and adjust console log

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
 async function getDashboardData(query) {
   //Chiamata Destinazione API
 
+  let object;
+
   //Promise
+
   const [responseDestination, responseMeteo, responseAirports] =
     await Promise.all([
       fetch(`http://localhost:3333/destinations?search=${query}`),
@@ -10,28 +13,52 @@ async function getDashboardData(query) {
     ]);
 
   //Risposte Array
-  const [destinationData, dataMeteo, dataAirports] = await Promise.all([
-    responseDestination.json(),
-    responseMeteo.json(),
-    responseAirports.json(),
-  ]);
+
+  try {
+    const [destinationData, dataMeteo, dataAirports] = await Promise.all([
+      responseDestination.json(),
+      responseMeteo.json(),
+      responseAirports.json(),
+    ]);
+
+    object = {
+      cityName: destinationData[0] ? destinationData[0].name : null,
+      cityCountry: destinationData[0] ? destinationData[0].country : null,
+      cityTemperature: dataMeteo[0] ? dataMeteo[0].temperature : null,
+      cityWeather: dataMeteo[0] ? dataMeteo[0].weather_description : null,
+      cityAirport: dataAirports[0] ? dataAirports[0].name : null,
+    };
+  } catch (error) {
+    console.log(object);
+    console.error(error);
+  }
+
+  //   const [destinationData, dataMeteo, dataAirports] = await Promise.all([
+  //     responseDestination.json(),
+  //     responseMeteo.json(),
+  //     responseAirports.json(),
+  //   ]);
 
   return {
-    cityName: destinationData[0].name,
-    cityCountry: destinationData[0].country,
-    cityTemperature: dataMeteo[0].temperature,
-    cityWeather: dataMeteo[0].weather_description,
-    cityAirport: dataAirports[0].name,
+    object,
   };
 }
 
-getDashboardData('london')
+getDashboardData('vienna')
   .then((data) => {
     console.log('Dashboard Data', data);
-    console.log(
-      `La città che hai cercato è ${data.cityName}, si trova in ${data.cityCountry}.\n` +
-        `Oggi la temperatura è di ${data.cityTemperature} gradi, ed il tempo è ${data.cityWeather}.\n` +
-        `L'aereoporto principale è ${data.cityAirport}`,
-    );
+
+    if (data.object.cityTemperature === null) {
+      console.log(
+        `La città che hai cercato è ${data.object.cityName}, si trova in ${data.object.cityCountry}.\n` +
+          `L'aereoporto principale è ${data.object.cityAirport}`,
+      );
+    } else {
+      console.log(
+        `La città che hai cercato è ${data.object.cityName}, si trova in ${data.object.cityCountry}.\n` +
+          `Oggi la temperatura è di ${data.object.cityTemperature} gradi, ed il tempo è ${data.object.cityWeather}.\n` +
+          `L'aereoporto principale è ${data.object.cityAirport}`,
+      );
+    }
   })
   .catch((error) => console.error(error));
